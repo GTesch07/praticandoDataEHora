b5c61d40129494c19659c6dd1a71090e021d55d0
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-06-19 19:11:35 -0300

    Primeiro commit trabalhando com Datas.

---
+++ src/Principal/Principal.java
@@ -0,0 +1,16 @@
+package Principal;
+
+import java.time.LocalDate;
+
+public class Principal {
+    public static void main(String[] args){
+        LocalDate dataCompra = LocalDate.now();
+        LocalDate dataPrimeiraParcela = LocalDate.of(2025, 5, 15);
+        LocalDate dataSegundaParcela = dataPrimeiraParcela.plusDays(30);
+
+        System.out.println("Data compra: " + dataCompra);
+        System.out.println("Data primeira parcela: " + dataPrimeiraParcela);
+        System.out.println("Data segunda parcela: " + dataSegundaParcela);
+    }
+
+}
