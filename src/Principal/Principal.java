9d72dbd08aaf99de521b02de29de6a3c22b76287
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-06-19 19:19:11 -0300

    Adicionando If-Else que verifica se data de vencimento da parcela.

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -5,8 +5,16 @@ import java.time.LocalDate;
 public class Principal {
     public static void main(String[] args){
         LocalDate dataCompra = LocalDate.now();
-        LocalDate dataPrimeiraParcela = LocalDate.of(2025, 5, 15);
+        LocalDate dataPrimeiraParcela = LocalDate.of(2025, 10, 19);
         LocalDate dataSegundaParcela = dataPrimeiraParcela.plusDays(30);
+
+        if (dataPrimeiraParcela.isEqual(LocalDate.now())){
+            System.out.println("Hoje é o dia do vencimento");
+        }else if (dataPrimeiraParcela.isAfter(LocalDate.now())) {
+            System.out.println("Ainda não está no dia do vencimento");
+        }else {
+            System.out.println("Parcela vencida!!!");
+        }
 
         System.out.println("Data compra: " + dataCompra);
         System.out.println("Data primeira parcela: " + dataPrimeiraParcela);
